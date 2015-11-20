4b133649866445954ac089d0456441f52821cd24
lingyu <lingyu@wacai.com>
2015-11-21 00:13:35 +0800

    project init

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var webpack = require('webpack');
+
+module.exports = {
+    // plugins: {commonsPlugin},
+
+    entry: {
+        index: './src/js/index.js'
+    },
+
+    output: {
+        path: './dist/js',
+        filename: '[name].bundle.js'
+    },
+
+    module: {
+        loaders: [
+            { test: /\.css$/,           loader: 'style-loader!css-loader' },
+            { test: /\.js$/,            loader: 'jsx-loader?harmony' },
+            { test: /\.scss$/,          loader: 'style!css!sass?sourceMap' },
+            { test: /\.less$/,          loader: 'style!css!less' },
+            { test: /\.(.png|.jpg)$/,   loader: 'url-loader?limit=8192' }
+        ]
+    },
+
+    resolve: {
+        extensions: ['','.js', '.json', '.scss', '.less']
+    }
+}
