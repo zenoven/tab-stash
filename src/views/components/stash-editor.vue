551eb7b5c4ff3b4352eab2a3577c8166e355c97c
lingyu <lingyu@wacai.com>
2016-07-23 18:57:36 +0800

    finish modify stash

--- src/views/components/stash-editor.vue
+++ src/views/components/stash-editor.vue
@@ -1,19 +1,33 @@
 <template>
-    <div class="title-edit-wrapper" :class="{ 'show' : active }">
+    <div class="title-edit-wrapper" :class="{ 'show' : active }" @click.self="reset">
         <div class="editor-wrapper">
             <input class="ipt-title" type="text" v-model="title" />
         </div>
     </div>
 </template>
 <script>
+    import stash from '../../js/lib/stash';
     export default {
         props: ['currentStashIndex'],
         computed: {
-            title: function () {
-                return this.currentStashIndex == -1 ? '' : this.$root.list[this.currentStashIndex].title
+            title: {
+                get(){
+                    return this.currentStashIndex == -1 ? '' : this.$root.list[this.currentStashIndex].title
+                },
+                set(newVal){
+                    var stashItem = this.$root.list[this.currentStashIndex];
+                    stash.modify(stashItem.id, newVal, function () {
+                        stashItem.title = newVal;
+                    })
+                }
             },
             active: function () {
                 return this.currentStashIndex != -1 ;
+            }
+        },
+        methods: {
+            reset: function () {
+                this.$root.currentStashIndex = -1;
             }
         }
 
